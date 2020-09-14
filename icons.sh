77738ce372b85aac7168ad716aa6f1c78ec7a9e6
JavaGT <javagrant@gmail.com>
2020-09-15 01:17:32 +1200

    content

--- icons.sh
+++ icons.sh
@@ -1,5 +1,15 @@
 # sh icons.sh will create a smaller, compressed image of all files in /img in /icon with the same file name.
 # echo ./img/**/**/*.jpg
+for f in ./pic/*.jpg; do
+  mkdir -p "$(dirname "./img${f:5}")" && convert -define jpeg:extent=3Mb ./"$f" ./img"${f:5}"
+done
+for f in ./pic/**/*.jpg; do
+  mkdir -p "$(dirname "./img${f:5}")" && convert -define jpeg:extent=3Mb ./"$f" ./img"${f:5}"
+done
+for f in ./pic/**/**/*.jpg; do
+  mkdir -p "$(dirname "./img${f:5}")" && convert -define jpeg:extent=3Mb ./"$f" ./img"${f:5}"
+done
+
 for f in ./img/*.jpg; do
   mkdir -p "$(dirname "./icon${f:5}")" && convert -define jpeg:extent=256kb -resize 300x ./"$f" ./icon"${f:5}"
 done
