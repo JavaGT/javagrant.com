c82dc3495e5b106ad82adb7e4a42f83b032a2f65
JavaGT <javagrant@gmail.com>
2020-11-19 00:45:11 +1300

    better grid

--- build_tools/image_gallery/build_webpage.mjs
+++ build_tools/image_gallery/build_webpage.mjs
@@ -3,12 +3,12 @@ import pug from "pug";
 import { fileURLToPath } from "url";
 import path from "path";
 
-const render = pug.compileFile(
-  path.join(path.dirname(fileURLToPath(import.meta.url)), "template.pug")
-);
 
 // const render = pug.compileFile('./template.pug')
 export default async function generateHTML(tree, root) {
+    const render = pug.compileFile(
+      path.join(path.dirname(fileURLToPath(import.meta.url)), "template.pug")
+    );
     const css = (await fs.readFile(
       path.join(path.dirname(fileURLToPath(import.meta.url)), "style.css")
     )).toString()
