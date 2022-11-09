f14526a06a7ea204e5bf4c5e57494e2c7629baad
Java <java@javagrant.com>
2022-11-09 22:14:10 +1300

    improved vis

--- source/static/three/plugins/Rainbow3D.mjs
+++ source/static/three/plugins/Rainbow3D.mjs
@@ -20,9 +20,12 @@ export default class Rainbow3D extends Plugin3D {
     }
     animate({ timestamp, data }) {
         this.cubes.forEach((cube, i) => {
-            cube.scale.y = (0.5 + (data[i] / 100)) ** 2
+            cube.scale.y = ((0.5 + (data[i] / 100)) ** 2) * 2
             // cube.scale.y = (1 + ((data[Math.floor(i * 1000 / 100)] - 128) / 128) * 50 + cube.scale.y * 5) / 6
-            cube.position.x = -50 + Math.sin(timestamp / 3000) * 50 - 0.5 + i * 1.01
+            // cube.position.x = -50 + Math.sin(timestamp / 3000) * 50 - 0.5 + i * 1.01
+            let width = 1.05
+            let total_width = this.cubes.length * width
+            cube.position.x = (total_width - ((timestamp / 100) + (width * i)) % total_width) - total_width / 2//(/ 2 - i * width)
             cube.rotation.x = Math.sin(timestamp / 3000) + i * Math.cos(timestamp / 3000) / 4
             // const dataval = Math.abs(data[Math.floor(i * 1024 / this.cubes.length)] - 128)
             const dataval = (data[Math.floor(i * 1024 / this.cubes.length)] / 100 + cube.scale.y * 1) / 2
